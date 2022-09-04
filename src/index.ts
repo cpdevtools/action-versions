0a2034acfe2ab9a53a7864a62262faf7a7c8de82
Dan Erdelt <erd@conceptual.ca>
2022-09-04 08:19:19 -0400

    ...

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,24 @@
-import { getInput } from '@actions/core';
+import { getInput, setOutput } from '@actions/core';
 import { context } from '@actions/github';
 import { existsSync, readFileSync } from 'fs';
+import semver from 'semver'
+
+
+interface VersionOutputs {
+    branch: string;
+    isReleaseSourceBranch: boolean;
+    isReleaseTargetBranch: boolean;
+    version?: string;
+    versionMajor?: number;
+    versionMinor?: number;
+    versionPatch?: number;
+    versionBuild?: string[];
+    versionPrerelease?: string;
+    versionPrereleaseBuild?: number;
+    versionIsPrerelease?: boolean;
+    versionIsStable?: boolean;
+}
+
 
 
 
@@ -11,35 +29,49 @@ function getBranchMeta(branch: string) {
     const version = isReleaseBranch ? scope.slice(8) : (isPrepBranch ? scope.slice(2) : undefined);
     return {
         branch,
-        isReleaseBranch,
-        isPrepBranch,
+        isReleaseTargetBranch: isReleaseBranch,
+        isReleaseSourceBranch: isPrepBranch,
         version
     };
 }
 
 
 const branch = context.ref.slice("refs/heads/".length);
-
-/*
-const releaseScope = branch === 'main' || branch === 'master' ? 'v/latest' : branch;
-
-const isStagingScope = releaseScope.startsWith('v/');
-const scopeVersion = isStagingScope ? releaseScope.slice(2) : undefined;
-*/
-
 const branchMeta = getBranchMeta(branch);
 
 const versionFile = getInput('version-file', { trimWhitespace: true });
-
-
-console.log('branch', branch);
-console.log('branchMeta', branchMeta);
-
-console.log('versionFile', versionFile);
-console.log('exists', existsSync(versionFile));
-
+let ver: semver.SemVer | null = null;
 if (existsSync(versionFile)) {
     const verObj = JSON.parse(readFileSync(versionFile, { encoding: 'utf-8' }));
     const verStr = (verObj.version ?? '') as string;
-    console.log('version', verStr);
+    ver = semver.parse(verStr);
 }
+
+
+const out: VersionOutputs = {
+    branch: branchMeta.branch,
+    isReleaseSourceBranch: branchMeta.isReleaseSourceBranch,
+    isReleaseTargetBranch: branchMeta.isReleaseTargetBranch,
+    version: ver?.version ?? undefined,
+    versionMajor: ver?.major ?? undefined,
+    versionMinor: ver?.minor ?? undefined,
+    versionPatch: ver?.patch ?? undefined,
+    versionBuild: ver?.build as string[] | undefined,
+    versionPrerelease: (ver?.prerelease[0] as string) ?? undefined,
+    versionPrereleaseBuild: (ver?.prerelease[1] as number) ?? undefined,
+    versionIsPrerelease: !!ver?.prerelease.length,
+    versionIsStable: !ver?.prerelease.length && (ver?.major ?? 0) >= 1,
+};
+
+
+
+const table: {key:string, value: string | number | boolean | string[] | undefined}[] = [];
+
+Object.keys(out).forEach((k) => {
+    const key = k as keyof VersionOutputs;
+    const value = out[key];
+    setOutput(key, value);
+    table.push({key, value});
+});
+
+console.table(table)
