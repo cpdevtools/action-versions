e5702e98e3403eb4f297ebe444bf474edfb7bb0b
Dan Erdelt <erd@conceptual.ca>
2022-09-04 13:45:48 -0400

    ...

--- src/index.ts
+++ src/index.ts
@@ -1,52 +1,11 @@
-import { getInput, getMultilineInput, setOutput } from '@actions/core';
-import { context } from '@actions/github';
-import { existsSync, readFileSync } from 'fs';
-import semver from 'semver';
-import simpleGit from 'simple-git';
-import { evaluateVersion } from './evaluateVersion';
+import { setOutput } from '@actions/core';
+import { inspectVersion } from './inspectVersion';
 import { VersionEvaluation } from './VersionEvaluation';
 
 
-const branchInput = getInput('branch', { trimWhitespace: true }) || undefined;
-const versionFileInput = getInput('versionFile', { trimWhitespace: true }) || undefined;
-const versionInput = getInput('version', { trimWhitespace: true }) || undefined;
-const existingVersionsInput = getMultilineInput('existingVersions', { trimWhitespace: true });
-
-
-console.log('branchInput', branchInput);
-console.log('versionFileInput', versionFileInput);
-console.log('versionInput', versionInput);
-console.log('existingVersionsInput', existingVersionsInput);
-
 
 (async () => {
-    const git = simpleGit('.');
-
-    const branch = branchInput ?? context.ref.startsWith("refs/heads/") ? context.ref.slice(11) : undefined;
-
-    let ver: semver.SemVer | null = null;
-    if (versionInput !== undefined && versionFileInput === undefined) {
-        ver = semver.parse(versionInput);
-    } else {
-        const versionFile = versionFileInput ?? './package.json';
-        if (existsSync(versionFile)) {
-            const verObj = JSON.parse(readFileSync(versionFile, { encoding: 'utf-8' }));
-            const verStr = (verObj.version ?? '-') as string;
-            ver = semver.parse(verStr);
-        }
-        if (ver === null && versionInput !== undefined) {
-            ver = semver.parse(versionInput);
-        }
-    }
-    ver ??= semver.parse('0.0.0');
-
-    let existingVerStrings = existingVersionsInput.length ? existingVersionsInput : (await git.tags()).all;
-
-    let existingVersions = existingVerStrings
-        .map(i => semver.parse(i))
-        .filter(i => !!i) as semver.SemVer[];
-
-    const out = evaluateVersion(ver!, existingVersions, branch);
+    const out = await inspectVersion();
 
     const table: { key: string, value: string | number | boolean | string[] | undefined }[] = [];
     Object.keys(out).forEach((k) => {
@@ -57,4 +16,5 @@ console.log('existingVersionsInput', existingVersionsInput);
     });
     console.table(table);
 })();
+
 
---
+++ src/inspectVersion.ts
@@ -0,0 +1,41 @@
+import { getInput, getMultilineInput } from '@actions/core';
+import { context } from '@actions/github';
+import { existsSync, readFileSync } from 'fs';
+import semver from 'semver';
+import simpleGit from 'simple-git';
+import { evaluateVersion } from './evaluateVersion';
+
+export async function inspectVersion() {
+    const branchInput = getInput('branch', { trimWhitespace: true }) || undefined;
+    const versionFileInput = getInput('versionFile', { trimWhitespace: true }) || undefined;
+    const versionInput = getInput('version', { trimWhitespace: true }) || undefined;
+    const existingVersionsInput = getMultilineInput('existingVersions', { trimWhitespace: true });
+
+    const git = simpleGit('.');
+
+    const branch = branchInput ?? context.ref.startsWith("refs/heads/") ? context.ref.slice(11) : undefined;
+
+    let ver: semver.SemVer | null = null;
+    if (versionInput !== undefined && versionFileInput === undefined) {
+        ver = semver.parse(versionInput);
+    } else {
+        const versionFile = versionFileInput ?? './package.json';
+        if (existsSync(versionFile)) {
+            const verObj = JSON.parse(readFileSync(versionFile, { encoding: 'utf-8' }));
+            const verStr = (verObj.version ?? '-') as string;
+            ver = semver.parse(verStr);
+        }
+        if (ver === null && versionInput !== undefined) {
+            ver = semver.parse(versionInput);
+        }
+    }
+    ver ??= semver.parse('0.0.0');
+
+    let existingVerStrings = existingVersionsInput.length ? existingVersionsInput : (await git.tags()).all;
+
+    let existingVersions = existingVerStrings
+        .map(i => semver.parse(i))
+        .filter(i => !!i) as semver.SemVer[];
+
+    return evaluateVersion(ver!, existingVersions, branch);
+}
