ebdb8f1db61f06ab64a5d3517f3e5b5e3c1e2589
Dan Erdelt <erd@conceptual.ca>
2022-09-03 23:44:57 -0400

    ..

--- src/index.ts
+++ src/index.ts
@@ -4,19 +4,37 @@ import { existsSync, readFileSync } from 'fs';
 
 
 
+function getBranchMeta(branch: string) {
+    const scope = branch === 'main' || branch === 'master' ? 'v/latest' : branch;
+    const isReleaseBranch = scope.startsWith('release/');
+    const isPrepBranch = scope.startsWith('v/');
+    const version = isReleaseBranch ? scope.slice(8) : (isPrepBranch ? scope.slice(2) : undefined);
+    return {
+        branch,
+        isReleaseBranch,
+        isPrepBranch,
+        version
+    };
+}
+
+
 const branch = context.ref.slice("refs/heads/".length);
+
+/*
 const releaseScope = branch === 'main' || branch === 'master' ? 'v/latest' : branch;
 
 const isStagingScope = releaseScope.startsWith('v/');
 const scopeVersion = isStagingScope ? releaseScope.slice(2) : undefined;
+*/
+
+const branchMeta = getBranchMeta(branch);
 
 const versionFile = getInput('version-file', { trimWhitespace: true });
 
 
 console.log('branch', branch);
-console.log('releaseScope', releaseScope);
-console.log('scopeVersion', scopeVersion);
-console.log('isStagingScope', isStagingScope);
+console.log('branchMeta', branchMeta);
+
 console.log('versionFile', versionFile);
 console.log('exists', existsSync(versionFile));
 
