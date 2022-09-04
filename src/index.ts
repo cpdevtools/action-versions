84472ee97e3e4297a3b70a9df1544233740984e5
Dan Erdelt <erd@conceptual.ca>
2022-09-03 23:24:51 -0400

    ...

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,26 @@
-//import {} from '@actions/core'
-import {context} from '@actions/github'
+import { getInput } from '@actions/core';
+import { context } from '@actions/github';
+import { existsSync, readFileSync } from 'fs';
+
 
 
 const branch = context.ref.slice("refs/heads/".length);
+const releaseScope = branch === 'main' || branch === 'master' ? 'v/latest' : branch;
+
+const isStagingScope = releaseScope.startsWith('v/');
+const scopeVersion = isStagingScope ? releaseScope.slice(2) : undefined;
+
+const versionFile = getInput('version-file', { trimWhitespace: true });
 
 
 console.log('branch', branch);
+console.log('releaseScope', releaseScope);
+console.log('isStagingScope', isStagingScope);
+console.log('versionFile', versionFile);
+console.log('exists', existsSync(versionFile));
+
+if (existsSync(versionFile)) {
+    const verObj = JSON.parse(readFileSync(versionFile, { encoding: 'utf-8' }));
+    const verStr = (verObj.version ?? '') as string;
+    console.log('version', verStr);
+}
