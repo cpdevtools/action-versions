1baaa8ab7fd0ca9fe68129ba5e6db5e6880f3faf
Dan Erdelt <erd@conceptual.ca>
2022-09-05 09:50:38 -0400

    ...

--- src/compareVersions.ts
+++ src/compareVersions.ts
@@ -2,10 +2,14 @@ import semver from 'semver';
 import { prereleaseComapreValues } from './prereleaseComapreValues';
 
 export function compareVersions(a: semver.SemVer, b: semver.SemVer): number {
-    const aB = new semver.SemVer(a.version.split('-')[0]);
-    const bB = new semver.SemVer(b.version.split('-')[0]);
+    a ??= new semver.SemVer('0.0.0');
+    b ??= new semver.SemVer('0.0.0');
+
+    const aB = semver.parse(a.version.split('-')[0])!;
+    const bB = semver.parse(b.version.split('-')[0])!;
+
     let comp = semver.compare(aB, bB);
-    if (comp === 0 && a.prerelease?.length && b.prerelease?.length) {
+    if (comp === 0 && a?.prerelease?.length && b?.prerelease?.length) {
         const aV = prereleaseComapreValues[a.prerelease[0] as keyof typeof prereleaseComapreValues] ?? prereleaseComapreValues.dev;
         const bV = prereleaseComapreValues[b.prerelease[0] as keyof typeof prereleaseComapreValues] ?? prereleaseComapreValues.dev;
         return aV === bV ? 0 : (aV > bV ? 1 : -1);
