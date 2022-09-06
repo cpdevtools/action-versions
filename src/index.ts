988b8d42696d48c011f8dc8f47901202a21f42ab
Dan Erdelt <erd@conceptual.ca>
2022-09-06 09:57:40 -0400

    fail properties

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,47 @@
-import { setOutput, getBooleanInput, setFailed } from '@actions/core';
+import { getBooleanInput, setFailed, setOutput } from '@actions/core';
+import { VersionStatus } from 'VersionStatus';
 import { inspectVersion } from './inspectVersion';
 import { VersionEvaluation } from './VersionEvaluation';
 
-const failIfNotValidNew = getBooleanInput('failIfNotValidNew');
+type ValidationProperties =
+    'validCanCreate' |
+    'validIsNewVersion' |
+    'validIsHighestVersionInBranch' |
+    'validIsHighestVersion' |
+    'vaildBranchVersionMaximum' |
+    'validBranchVersionMinimum';
+
+const ValidationErrorMessages = {
+    validCanCreate: (data: VersionStatus) => {
+        let msg = `Error: ${data.targetVersion} is not a valid version.`;
+        if (data.targetVersion === data.sourceVersion) {
+            msg = `Error: ${data.targetVersion} is not a new version.`;
+        }
+        return msg;
+    },
+    validIsNewVersion: (data: VersionStatus) => {
+        return `Error: ${data.targetVersion} is not a new version.`;
+    },
+    validIsHighestVersionInBranch: (data: VersionStatus) => {
+        return `Error: Target version should be the higest version in the branches range. ${data.targetVersion} > ${data.sourceVersion}`;
+    },
+    validIsHighestVersion: (data: VersionStatus) => {
+        return `Error: ${data.targetVersion} is not the highest version. ${data.targetVersion} > ${data.highestVersion}`;
+    },
+    vaildBranchVersionMaximum: (data: VersionStatus) => {
+        return `Error: ${data.targetVersion} does not meet the minimum restriction for branch ${data.branch}`;
+    },
+    validBranchVersionMinimum: (data: VersionStatus) => {
+        return `Error: ${data.targetVersion} exceeds the maximum restriction for branch ${data.branch}`;
+    }
+}
+
+function applyFailed(validationProp: ValidationProperties, data: VersionStatus) {
+    const shouldFail = getBooleanInput(`failIn${validationProp}`);
+    if (shouldFail && !data[validationProp]) {
+        setFailed(ValidationErrorMessages[validationProp](data));
+    }
+}
 
 (async () => {
     const out = await inspectVersion();
@@ -16,13 +55,12 @@ const failIfNotValidNew = getBooleanInput('failIfNotValidNew');
     });
     console.table(table);
 
-    if(failIfNotValidNew && !out.validCanCreate){
-        let msg = `Error: ${out.targetVersion} is not a valid version.`;
-        if(out.targetVersion === out.sourceVersion){
-            msg = `Error: ${out.targetVersion} is not a new version.`;
-        }
-        setFailed(msg);
-    }
+    applyFailed('validCanCreate', out);
+    applyFailed('validIsNewVersion', out);
+    applyFailed('validIsHighestVersionInBranch', out);
+    applyFailed('validBranchVersionMinimum', out);
+    applyFailed('vaildBranchVersionMaximum', out);
+    applyFailed('validIsHighestVersion', out);
 })();
 
 
