bfec6e5858ad18e615c8963979184f731b257291
Dan Erdelt <erd@conceptual.ca>
2022-09-06 16:48:42 -0400

    ...

--- src/evaluateVersion.ts
+++ src/evaluateVersion.ts
@@ -88,6 +88,7 @@ export function evaluateVersion(targetVersion: semver.SemVer, existingVersions: 
     
     const existingHighestMajor = existingVersions.find(v => v.major === targetVersion.major) ?? new semver.SemVer('0.0.0');
     const highestMajor = semver.gte(targetVersion, existingHighestMajor);
+    console.log('highestMajor', targetVersion.version, existingHighestMajor.version, semver.gte(targetVersion, existingHighestMajor));
 
     const existingHighestMinor = existingVersions.find(v => v.major === targetVersion.major && v.minor === targetVersion.minor) ?? new semver.SemVer('0.0.0');
     const highestMinor = semver.gte(targetVersion, existingHighestMinor);
