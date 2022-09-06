76378d7774aaff28fe89bdd2f9ed482d81028b53
Dan Erdelt <erd@conceptual.ca>
2022-09-06 15:17:20 -0400

    ...

--- dist/install.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const child_process_1 = require("child_process");
-(0, child_process_1.spawn)(`npm i -g pnpm@7.6.0`, { shell: true, stdio: 'inherit' }).on('exit', () => {
-    (0, child_process_1.spawn)(`pnpm i --frozen-lockfile --ignore-scripts`, { shell: true, stdio: 'inherit' });
-});
--- dist/prereleaseComapreValues.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.prereleaseComapreValues = void 0;
-exports.prereleaseComapreValues = {
-    dev: 0,
-    alpha: 1,
-    beta: 2,
-    rc: 3,
-};
